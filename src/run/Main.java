35d8b2e87e7411d24ac6377c9e1ca292d8ed01fc
Ralitsa <ralitsa.simeonova933@gmail.com>
2018-06-06 13:17:54 +0300

    changes to Main class

--- src/run/Main.java
+++ src/run/Main.java
@@ -2,7 +2,7 @@ package run;
 
 import java.util.Scanner;
 
-import exceptions.WrongUserMenuInputException;
+import exceptions.WrongMenuInputException;
 
 public class Main {
 	
@@ -12,86 +12,56 @@ public class Main {
 		int inputOption = sc.nextInt(); 
 		try {
 			manageUserInput(inputOption);
-		} catch (WrongUserMenuInputException e) {
-			// TODO Replace with a more user-friendly message.
-			e.printStackTrace();
+		} catch (WrongMenuInputException e) {
+			System.out.println(e.getMessage());
 		}
 		
 		sc.close();
 	}
 		
-	public static void chooseMenuOption() {
-		//TODO These are client-side functionalities. The server application needs to have the administrative ones
 		//listed in issue #1
-		System.out.println("For login, press 0: ");
-		System.out.println("For creating account, press 1: ");
-		System.out.println("For starting new chat conversation, press 2: ");
-		System.out.println("To invite user in the chat group, press 3: ");
-		System.out.println("To join existing chat group, press 4: ");
-		System.out.println("To leave the chat group, press 5: ");
-		System.out.println("For logout, press 6: ");
-	}
+		public static void chooseMenuOption() {
+			System.out.println("To create user, press 0: ");
+			System.out.println("To delete user, press 1: ");
+			System.out.println("To list all users, press 2: ");
+			System.out.println("To exit, press 3: ");
+		}
 	
-	public static void manageUserInput(int inputOption) throws WrongUserMenuInputException {
+	public static void manageUserInput(int inputOption) throws WrongMenuInputException {
 		switch (inputOption) {
-		case 0: 
-			userLogin();
+		case 0:
+			createUser();
 			break;
 		case 1:
-			createAccount();
+			deleteUser();
 			break;
 		case 2:
-			startConversation();
+			listAllUsers();
 			break;
 		case 3:
-			inviteUser();
-			break;
-		case 4:
-			joinGroupChat();
-			break;
-		case 5:
-			leaveChatGroup();
-			break;
-		case 6:
-			userLogout();
+			exit();
 			break;
 		default:
-			throw new WrongUserMenuInputException("Choose a valid menu option!");
+			throw new WrongMenuInputException("Choose a valid menu option!");
 		}
 	}
 
-	private static void leaveChatGroup() {
+	private static void exit() {
 		// TODO Auto-generated method stub
 		
 	}
 
-
-	private static void userLogout() {
+	private static void listAllUsers() {
 		// TODO Auto-generated method stub
 		
 	}
 
-	private static void joinGroupChat() {
+	private static void deleteUser() {
 		// TODO Auto-generated method stub
 		
 	}
 
-	private static void inviteUser() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	private static void startConversation() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	private static void createAccount() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	private static void userLogin() {
+	private static void createUser() {
 		// TODO Auto-generated method stub
 		
 	}
