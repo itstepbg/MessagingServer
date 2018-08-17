5133bbeb4dcc41dcd6c12c9d6b0ad8857a1d8de3
Ralitsa Simeonova <r.simeonova@seeburger.com>
2018-08-17 11:18:20 +0300

    Insertion of non-existing users and files in sql table is no longer possible. Verifications are added in the share file case.

--- src/networking/ServerCommunication.java
+++ src/networking/ServerCommunication.java
@@ -2,6 +2,8 @@ package networking;
 
 import java.io.File;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
 import library.models.data.Directory;
@@ -256,19 +258,33 @@ public class ServerCommunication extends Communication {
 			sendMessage(statusMessage);
 			break;
 		case SHARE_FILE:
-
-			userId = UserManager.getInstance().insertSharedFileInfo(networkMessage.getUser(),
-					UserManager.getInstance().getLoggedInUser(this.userId).getName(), networkMessage.getFileName(),
-					networkMessage.getFilePath());
-
 			statusMessage = new NetworkMessage();
 			statusMessage.setType(MessageType.STATUS_RESPONSE);
 
 			String userNameSharedTo = networkMessage.getUser();
-			String filePathSharedFile = networkMessage.getFilePath();
+			boolean userExists = false;
+			for (User user : UserManager.getInstance().getAllUsers()) {
+				if (user.getName().equals(userNameSharedTo)) {
+					userExists = true;
+					statusMessage.setStatus(NetworkMessage.STATUS_OK);
+					break;
+				}
+			}
+			if (!userExists) {
+				statusMessage.setStatus(NetworkMessage.STATUS_NON_EXISTING_USER);
+			}
+			String filePathSharedFile = UserManager.USER_FILES_DIRECTORY
+					+ UserManager.getInstance().getLoggedInUser(this.userId).getName() + "\\"
+					+ networkMessage.getFilePath();
+			if (!Files.exists(Paths.get(filePathSharedFile))) {
+				statusMessage.setStatus(NetworkMessage.STATUS_ERROR_SHARING_FILE);
+			}
 
-			statusMessage.setStatus(NetworkMessage.STATUS_OK);
-
+			if (statusMessage.getStatus().equals(NetworkMessage.STATUS_OK)) {
+				userId = UserManager.getInstance().insertSharedFileInfo(networkMessage.getUser(),
+						UserManager.getInstance().getLoggedInUser(this.userId).getName(), networkMessage.getFileName(),
+						networkMessage.getFilePath());
+			}
 			statusMessage.setMessageId(networkMessage.getMessageId());
 			sendMessage(statusMessage);
 			break;
